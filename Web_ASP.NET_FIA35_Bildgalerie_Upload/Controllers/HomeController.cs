e278e0d0b75780de428280c1fe2008d61c867d65
Thorsten Diederichs <DiederichsT@bfwob.de>
2021-10-13 13:18:56 +0200

    Änderungen

--- Web_ASP.NET_FIA35_Bildgalerie_Upload/Controllers/HomeController.cs
+++ Web_ASP.NET_FIA35_Bildgalerie_Upload/Controllers/HomeController.cs
@@ -9,24 +9,33 @@ using Web_ASP.NET_FIA35_Bildgalerie_Upload.Models;
 
 namespace Web_ASP.NET_FIA35_Bildgalerie_Upload.Controllers
 {
+    // Durch Eingabe von "ctor" und zweimal Tab wird ein Konstruktor einer Klasse in dieser Klasse erzeugt
+    //public ClassNamePlaceholder()
+    //{
+
+    //}
+
     public class HomeController : Controller
     {
+        // Liste der erlaubten MimeTypes
+        List<string> allowedMimeTypes = new List<string> { "image/png", "image/jpeg", "image/gif", "image/webp" };
+
         // Hierin befinden sich alle Informationen zur Umgebung des Webprojekts
         // "readonly" bewirkt, dass es keine Änderungen geben kann (außer durch den Konstruktor beim Erzeugen der neuen Instanz am Anfang)
         // "private" müsste nicht extra gesetzt werden, weil alle Methoden per Standard auf "private" gesetzt sind
-        private readonly IWebHostEnvironment Environment;
+        private readonly IWebHostEnvironment InfoUeberWebserver;
 
         public HomeController(IWebHostEnvironment _environment)  // Konstruktor ist "public", weil sonst keine Instanz von außen erzeugt wird
         {
-            Environment = _environment;
+            InfoUeberWebserver = _environment;
         }
 
         [HttpGet]
         public IActionResult Index()
         {
             // Umgebungsinformationen zur Pfadermittlung der Bilder nutzen
-            string wwwPath = this.Environment.WebRootPath;
-            string contentPath = this.Environment.ContentRootPath;
+            string wwwPath = this.InfoUeberWebserver.WebRootPath;
+            string contentPath = this.InfoUeberWebserver.ContentRootPath;
 
             DirectoryInfo di = new DirectoryInfo(wwwPath + "\\Bilder");
             FileInfo[] fi = di.GetFiles("*.*");
@@ -34,7 +43,7 @@ namespace Web_ASP.NET_FIA35_Bildgalerie_Upload.Controllers
 
             // Model mit Liste der Dateiinfos
             BilderListe bilderListe = new BilderListe { listDateiInfos = DateiListe };
-            
+
             return View(bilderListe);
         }
 
@@ -53,18 +62,6 @@ namespace Web_ASP.NET_FIA35_Bildgalerie_Upload.Controllers
         [HttpPost]
         public IActionResult Upload(Bildcs neuBild)
         {
-            string guid = Guid.NewGuid().ToString();
-            string wwwPath = this.Environment.WebRootPath;
-            string contentPath = this.Environment.ContentRootPath;
-            List<string> allowedMimeTypes = new List<string> { "image/png", "image/jpeg", "image/gif", "image/webp" };
-
-            // Falls der Ordner "Bilder" noch nicht vorhanden ist, wird er erzeugt
-            string path = Path.Combine(this.Environment.WebRootPath, "Bilder");
-            if (!Directory.Exists(path))
-            {
-                Directory.CreateDirectory(path);
-            }
-
             // Prüfen, ob MimeType erlaubt ist:
             if (!allowedMimeTypes.Contains(neuBild.file.ContentType))
             {
@@ -72,15 +69,34 @@ namespace Web_ASP.NET_FIA35_Bildgalerie_Upload.Controllers
                 return View();
             }
 
+            SpeichernBild(neuBild);
+
+            return View();
+        }
+
+        private void SpeichernBild(Bildcs neuBild)
+        {
+            string guid = Guid.NewGuid().ToString();
+            string wwwPath = this.InfoUeberWebserver.WebRootPath;
+            string contentPath = this.InfoUeberWebserver.ContentRootPath;
+
+            // Falls der Ordner "Bilder" noch nicht vorhanden ist, wird er erzeugt
+            string path = Path.Combine(this.InfoUeberWebserver.WebRootPath, "Bilder");
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            // Um Files wieder zu löschen
+            // System.IO.File.Delete("/wwwroot...");
+
             // Speichern / Uploaden
-            string neuFName = guid + neuBild.file.FileName;
+            string neuFName = guid + neuBild.file.FileName.Substring(neuBild.file.FileName.LastIndexOf("."));
             using (FileStream Fstream = new FileStream(wwwPath + "/Bilder/" + neuFName, FileMode.Create))
             {
                 neuBild.file.CopyTo(Fstream);
                 ViewBag.Dateiname = neuBild.file.FileName;
             }
-
-            return View();
         }
     }
 }
